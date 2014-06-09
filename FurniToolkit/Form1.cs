bae367c811c495c0ea9a8a9becfd7796be290890
AWA <awa500@hotmail.com>
2014-06-09 14:34:08 +0200

    Mostly done

--- FurniToolkit/Form1.cs
+++ FurniToolkit/Form1.cs
@@ -5,14 +5,25 @@ using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
+using FurniToolkit.Properties;
 
 namespace FurniToolkit
 {
     public partial class Form1 : Form
     {
+        public const string CONVERT_FILENAME = "furnidata_converted.xml";
+        public const string SYNC_FILENAME = "furnidata_synced.xml";
+
+        private string _sourceFilePath;
+        private string _syncFilePath;
+        private string _convertFilePath;
+
         public Form1()
         {
             InitializeComponent();
@@ -22,9 +33,222 @@ namespace FurniToolkit
         {
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
-                StreamReader sr = new StreamReader(openFileDialog1.FileName);
-                MessageBox.Show(sr.ReadToEnd());
-                sr.Close();
+                _sourceFilePath = openFileDialog1.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog2.ShowDialog() == DialogResult.OK)
+            {
+                _syncFilePath = openFileDialog2.FileName;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog3.ShowDialog() == DialogResult.OK)
+            {
+                _convertFilePath = openFileDialog3.FileName;
+            }
+        }
+
+        // Sync button
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            _sourceFilePath = textBox1.Text;
+            _syncFilePath = textBox2.Text;
+            if (string.IsNullOrWhiteSpace(_sourceFilePath) || string.IsNullOrWhiteSpace(_syncFilePath))
+            {
+                MessageBox.Show(Resources.Form1_button4_Click_Some_invalid_file_paths);
+                return;
+            }
+
+            button3.Enabled = false;
+            await Task.Factory.StartNew(delegate
+            {
+                List<Item> sourceItems = LoadFurnidata(_sourceFilePath);
+                List<Item> syncItems = LoadFurnidata(_syncFilePath);
+
+                foreach (Item item in syncItems)
+                {
+                    if (!sourceItems.Any(i => i.ID == item.ID && i.Type == item.Type))
+                        sourceItems.Add(item);
+                }
+                SaveFurnidata(sourceItems, SYNC_FILENAME);
+            });
+            button3.Enabled = true;
+            MessageBox.Show("Done syncing!");
+        }
+
+        // Convert button
+        private async void button4_Click(object sender, EventArgs e)
+        {
+            _convertFilePath = textBox3.Text;
+            if (string.IsNullOrWhiteSpace(_convertFilePath))
+            {
+                MessageBox.Show(Resources.Form1_button4_Click_Some_invalid_file_paths);
+                return;
+            }
+
+            button4.Enabled = false;
+            await Task.Factory.StartNew(delegate
+            {
+                List<Item> items = new List<Item>();
+                string data = "";
+                using (WebClient c = new WebClient())
+                    data = c.DownloadString(_convertFilePath);
+
+                string[] chunks = Regex.Split(data, "\n\r{1,}|\n{1,}|\r{1,}", RegexOptions.Multiline);
+                foreach (string chunk in chunks)
+                {
+                    MatchCollection collection = Regex.Matches(chunk, @"\[+?((.)*?)\]");
+                    foreach (Match item in collection)
+                    {
+                        items.Add(new Item(item.Value));
+                    }
+                }
+                SaveFurnidata(items, CONVERT_FILENAME);
+            });
+            button4.Enabled = true;
+            MessageBox.Show(Resources.Form1_button4_Click_Done_converting_);
+        }
+
+        private List<Item> LoadFurnidata(string path)
+        {
+            List<Item> items = new List<Item>();
+
+            XmlDocument xml = new XmlDocument();
+            xml.Load(path);
+            XmlNodeList itemNodes = xml.SelectNodes("//furnitype");
+
+            foreach (XmlNode node in itemNodes)
+            {
+                Item item = new Item(node);
+                if (!items.Any(i => i.ID == item.ID && i.Type == item.Type))
+                    items.Add(item);
+            }
+            //XmlReader reader = XmlReader.Create(path);
+            //char currentType = 's';
+            //while (reader.Read())
+            //{
+            //    if (reader.NodeType != XmlNodeType.Element)
+            //        continue;
+
+            //    if (reader.Name == "wallitemtypes")
+            //        currentType = 'i';
+
+
+            //    if (reader.Name == "furnitype")
+            //    {
+            //        Item item = new Item(reader, currentType);
+            //        if (item.ID == 13)
+            //        {
+                        
+            //        }
+            //        if(!items.ContainsKey(item.ID))
+            //            items.Add(item.ID, item);
+            //    }
+            //}
+            return items;
+        }
+
+        private void SaveFurnidata(List<Item> items, string path)
+        {
+            List<Item> floorItems = items.Where(t => t.Type == 's').ToList();
+            List<Item> wallItems = items.Where(t => t.Type == 'i').ToList();
+
+            using (XmlWriter writer = XmlWriter.Create(path))
+            {
+                writer.WriteStartDocument();
+                writer.WriteStartElement("furnidata");
+
+                writer.WriteStartElement("roomitemtypes");
+                foreach (Item i in floorItems)
+                {
+                    writer.WriteStartElement("furnitype");
+                    writer.WriteAttributeString("id", i.ID.ToString());
+                    writer.WriteAttributeString("classname", i.ClassName);
+
+                    writer.WriteElementString("revision", i.Revision.ToString());
+                    writer.WriteElementString("defaultdir", "0");
+                    writer.WriteElementString("xdim", i.TileSizeX.ToString());
+                    writer.WriteElementString("ydim", i.TileSizeY.ToString());
+
+                    writer.WriteStartElement("partcolors");
+                    foreach (string color in i.Colors)
+                    {
+                        writer.WriteStartElement("color");
+                        writer.WriteString(color);
+                        writer.WriteEndElement();
+                    }
+                    writer.WriteEndElement();
+
+                    writer.WriteElementString("name", i.Title);
+                    writer.WriteElementString("description", i.Description);
+                    writer.WriteElementString("adurl", i.AdURL);
+                    writer.WriteElementString("offerid", i.OfferID.ToString());
+                    writer.WriteElementString("buyout", Convert.ToInt32(i.Buyout).ToString());
+                    writer.WriteElementString("rentofferid", i.RentOfferID.ToString());
+                    writer.WriteElementString("rentbuyout", Convert.ToInt32(i.RentBuyout).ToString());
+                    writer.WriteElementString("bc", Convert.ToInt32(i.BuildersClub).ToString());
+                    writer.WriteElementString("customparams", i.CustomParams);
+                    writer.WriteElementString("specialtype", i.SpecialType.ToString());
+                    writer.WriteElementString("canstandon", Convert.ToInt32(i.CanStandOn).ToString());
+                    writer.WriteElementString("cansiton", Convert.ToInt32(i.CanSitOn).ToString());
+                    writer.WriteElementString("canlayon", Convert.ToInt32(i.CanLayOn).ToString());
+                    writer.WriteEndElement();
+                }
+                writer.WriteEndElement();
+
+                writer.WriteStartElement("wallitemtypes");
+                foreach (Item i in wallItems)
+                {
+                    writer.WriteStartElement("furnitype");
+                    writer.WriteAttributeString("id", i.ID.ToString());
+                    writer.WriteAttributeString("classname", i.ClassName);
+
+                    writer.WriteElementString("revision", i.Revision.ToString());
+                    //writer.WriteElementString("defaultdir", "0");
+                    //writer.WriteElementString("xdim", i.TileSizeX.ToString());
+                    //writer.WriteElementString("ydim", i.TileSizeY.ToString());
+
+                    //writer.WriteStartElement("partcolors");
+                    //foreach (string color in i.Colors)
+                    //{
+                    //    writer.WriteStartElement("color");
+                    //    writer.WriteString(color);
+                    //    writer.WriteEndElement();
+                    //}
+                    //writer.WriteEndElement();
+
+                    writer.WriteElementString("name", i.Title);
+                    writer.WriteElementString("description", i.Description);
+                    writer.WriteElementString("adurl", i.AdURL);
+                    writer.WriteElementString("offerid", i.OfferID.ToString());
+                    writer.WriteElementString("buyout", Convert.ToInt32(i.Buyout).ToString());
+                    writer.WriteElementString("rentofferid", i.RentOfferID.ToString());
+                    writer.WriteElementString("rentbuyout", Convert.ToInt32(i.RentBuyout).ToString());
+                    writer.WriteElementString("bc", Convert.ToInt32(i.BuildersClub).ToString());
+                    // writer.WriteElementString("customparams", i.CustomParams.ToString());
+                    writer.WriteElementString("specialtype", i.SpecialType.ToString());
+                    //writer.WriteElementString("canstandon", Convert.ToInt32(i.CanStandOn).ToString());
+                    //writer.WriteElementString("cansiton", Convert.ToInt32(i.CanSitOn).ToString());
+                    //writer.WriteElementString("canlayon", Convert.ToInt32(i.CanLayOn).ToString());
+                    writer.WriteEndElement();
+                    //var objects = new object[]
+                    //{
+                    //    i.ID, i.Type, i.Name, i.ColorIndex, i.Revision, i.TileSizeX, i.TileSizeY, i.TileSizeZ,
+                    //    String.Join(",", i.Colors), i.Title, i.Description, i.AdURL, i.CatalogPageID, i.OfferID
+                    //};
+                    //builder.AppendLine(
+                    //    string.Format(
+                    //        "ID: {0}, TYPE: {1}, NAME: {2}, COLORINDEX: {3}, REVISION: {4}, TILESIZEX: {5}, TILESIZEY: {6}, TILESIZEZ: {7}, COLORS: {8}, TITLE: {9}, DESCRIPTION: {10}, ADURL: {11}, CATALOGPAGEID: {12}, OFFERID: {13}",
+                    //        objects));
+                }
+                writer.WriteEndElement();
+
+                writer.WriteEndDocument();
             }
         }
     }
---
+++ FurniToolkit/Item.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace FurniToolkit
+{
+    class Item
+    {
+        public int ID;
+        public int Revision;
+        public int DefaultDir;
+        public char Type;
+        public string ClassName;
+        public int ColorIndex;
+        public int TileSizeX;
+        public int TileSizeY;
+        public int TileSizeZ;
+        public List<string> Colors;
+        public string Title;
+        public string Description;
+        public string AdURL;
+        public int CatalogPageID;
+
+        public int OfferID;
+        public int RentOfferID;
+
+        public bool Buyout;
+        public bool RentBuyout;
+
+        public string CustomParams;
+        public int SpecialType;
+        public bool BuildersClub;
+
+        public bool CanStandOn;
+        public bool CanSitOn;
+        public bool CanLayOn;
+
+
+        public Item(XmlNode node)
+        {
+            ID = int.Parse(node.Attributes["id"].InnerText);
+            ClassName = node.Attributes["classname"].InnerText;
+            Type = node.ParentNode.Name == "roomitemtypes" ? 's' : 'i';
+            Colors = new List<string>();
+            foreach (XmlNode child in node.ChildNodes)
+            {
+                switch (child.Name)
+                {
+                    case "revision":
+                        Revision = int.Parse(child.InnerXml);
+                        break;
+                    case "defaultdir":
+                        DefaultDir = int.Parse(child.InnerXml);
+                        break;
+                    case "xdim":
+                        TileSizeX = int.Parse(child.InnerXml);
+                        break;
+                    case "ydim":
+                        TileSizeY = int.Parse(child.InnerXml);
+                        break;
+                    case "color":
+                        Colors.Add(child.InnerXml);
+                        break;
+                    case "name":
+                        Title = child.InnerXml;
+                        break;
+                    case "description":
+                        Description = child.InnerXml;
+                        break;
+                    case "adurl":
+                        AdURL = child.InnerXml;
+                        break;
+                    case "offerid":
+                        OfferID = int.Parse(child.InnerXml);
+                        break;
+                    case "buyout":
+                        Buyout = child.InnerXml == "1";
+                        break;
+                    case "rentofferid":
+                        RentOfferID = int.Parse(child.InnerXml);
+                        break;
+                    case "rentbuyout":
+                        RentBuyout = child.InnerXml == "1";
+                        break;
+                    case "bc":
+                        BuildersClub = child.InnerXml == "1";
+                        break;
+                    case "customparams":
+                        CustomParams = child.InnerXml;
+                        break;
+                    case "specialtype":
+                        SpecialType = int.Parse(child.InnerXml);
+                        break;
+                    case "canstandon":
+                        CanStandOn = child.InnerXml == "1";
+                        break;
+                    case "cansiton":
+                        CanSitOn = child.InnerXml == "1";
+                        break;
+                    case "furnitype":
+                        return;
+                }
+            }
+
+            if (ClassName.Contains('*'))
+            {
+                ColorIndex = int.Parse(ClassName.Split('*')[1]);
+                ClassName = ClassName.Split('*')[0];
+            }
+            else
+                ColorIndex = -1;
+        }
+        public Item(string data)
+        {
+            if (string.IsNullOrWhiteSpace(data))
+                return;
+
+            data = Regex.Replace(data, @"/\[{1,}/mg", "");
+            data = Regex.Replace(data, @"/\[{1,}/mg", "");
+            List<string> splitted = data.Split('"').ToList();
+            //RemoveOnValue(splitted, ",", true);
+            splitted.RemoveAll(s => s.Trim() == ",");
+            splitted.RemoveAt(0);
+            splitted.RemoveAt(splitted.Count - 1);
+
+            Type = splitted[0][0];
+            ID = int.Parse(splitted[1]);
+            ClassName = splitted[2];
+            if (ClassName.Contains('*'))
+            {
+                ColorIndex = int.Parse(ClassName.Split('*')[1]);
+            }
+            else
+                ColorIndex = 0;
+
+            Revision = int.Parse(splitted[3]);
+            TileSizeX = string.IsNullOrWhiteSpace(splitted[4]) ? 1 : int.Parse(splitted[4]);
+            TileSizeY = string.IsNullOrWhiteSpace(splitted[5]) ? 1 : int.Parse(splitted[5]);
+            TileSizeZ = string.IsNullOrWhiteSpace(splitted[6]) ? 0 : int.Parse(splitted[6]);
+            Colors = splitted[7].Split(',').ToList();
+            Title = splitted[8];
+            Description = splitted[9];
+
+            // Fix if invalid stuff
+            TileSizeX = Math.Max(TileSizeX, 1);
+            TileSizeY = Math.Max(TileSizeY, 1);
+
+            // Set some default values
+            SpecialType = 1;
+            OfferID = -1;
+            RentOfferID = -1;
+            CustomParams = "";
+
+            // We usually have corruped stuff below, so we use try, catch
+            try
+            {
+                AdURL = splitted[10];
+                if (splitted.Count > 17)
+                {
+                    OfferID = int.Parse(splitted[11]);
+                    Buyout = bool.Parse(splitted[12]);
+                    RentOfferID = int.Parse(splitted[13]);
+                    RentBuyout = bool.Parse(splitted[14]);
+
+                    CustomParams = splitted[15];
+                    SpecialType = int.Parse(splitted[16]);
+                    BuildersClub = bool.Parse(splitted[17]);
+
+                    if (Type != 'i')
+                    {
+                        CanStandOn = bool.Parse(splitted[18]);
+                        CanSitOn = bool.Parse(splitted[19]);
+                        CanLayOn = bool.Parse(splitted[20]);
+                    }
+                }
+                else if (splitted.Count > 13)
+                {
+                    CatalogPageID = int.Parse(splitted[11]);
+                    OfferID = int.Parse(splitted[12]);
+                }
+            }
+            catch (Exception x)
+            {
+                //Console.WriteLine("Partly corruped item: '{0}'", FullName);
+            }
+        }
+    }
+}
